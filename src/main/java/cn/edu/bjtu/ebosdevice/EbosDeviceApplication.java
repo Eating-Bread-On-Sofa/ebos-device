09113f22f340a477d5ad60f6c88f0f0008b15b74
Cypo <voodoo.7@outlook.com>
2020-06-17 17:05:31 +0800

    change thresold of Read Time Out

--- src/main/java/cn/edu/bjtu/ebosdevice/EbosDeviceApplication.java
+++ src/main/java/cn/edu/bjtu/ebosdevice/EbosDeviceApplication.java
@@ -16,8 +16,8 @@ public class EbosDeviceApplication {
     @Bean
     public RestTemplate restTemplate() {
         SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
-        simpleClientHttpRequestFactory.setConnectTimeout(3000);
-        simpleClientHttpRequestFactory.setReadTimeout(1000);
+        simpleClientHttpRequestFactory.setConnectTimeout(10000);
+        simpleClientHttpRequestFactory.setReadTimeout(10000);
         RestTemplate restTemplate = new RestTemplate(simpleClientHttpRequestFactory);
         return restTemplate;
     }
